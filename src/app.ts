63fcc218b3f70a9f70ea36074d1993f9934c3636
harshit Mishra <miananya2@gmail.com>
2024-08-24 20:03:43 +0530

    starter

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+export default app;
